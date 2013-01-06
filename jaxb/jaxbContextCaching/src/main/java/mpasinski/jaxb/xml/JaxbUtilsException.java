37782fb361f78262074ef0c76db69305bd5abe00
Michal Pasinski <pasinski@gmail.com>
2013-01-06 19:51:04 +0100

    Adding first project

---
+++ jaxb/jaxbContextCaching/src/main/java/mpasinski/jaxb/xml/JaxbUtilsException.java
@@ -0,0 +1,25 @@
+package mpasinski.jaxb.xml;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mpasinski
+ * Date: 06.12.12
+ * Time: 21:57
+ * To change this template use File | Settings | File Templates.
+ */
+public class JaxbUtilsException extends RuntimeException {
+    public JaxbUtilsException() {
+    }
+
+    public JaxbUtilsException(String message) {
+        super(message);
+    }
+
+    public JaxbUtilsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public JaxbUtilsException(Throwable cause) {
+        super(cause);
+    }
+}
