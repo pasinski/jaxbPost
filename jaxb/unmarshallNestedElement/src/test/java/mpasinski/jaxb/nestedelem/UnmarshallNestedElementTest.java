20ea5cfdd8bf151aed9ae2255e51b817ecaf32ff
Michal Pasinski <pasinski@gmail.com>
2013-01-08 23:38:09 +0100

    prepared for unmarshall nested element post

---
+++ jaxb/unmarshallNestedElement/src/test/java/mpasinski/jaxb/nestedelem/UnmarshallNestedElementTest.java
@@ -0,0 +1,11 @@
+package mpasinski.jaxb.nestedelem;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mpasinski
+ * Date: 08.01.13
+ * Time: 23:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class UnmarshallNestedElementTest {
+}
